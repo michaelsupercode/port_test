7a0a33bd852353364c6ac96688f09ce635111bcb
michaelsupercode <96441437+michaelsupercode@users.noreply.github.com>
2024-06-09 18:23:43 +0200

    Update ContactSection.jsx

--- src/components/contact/ContactSection.jsx
+++ src/components/contact/ContactSection.jsx
@@ -3,7 +3,7 @@ import './ContactSection.scss';
 const ContactSection = () => {
   return (
     <section className="footerContainer">
-      <h2>eMail</h2>
+      <h2></h2>
       
       <a href="mailto:">eMail me</a>
     </section>
